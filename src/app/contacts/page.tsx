d0c8ddefdd4d9d733a849ba9dd0af2846eca16a7
Pedro Nunes <adm.pedronunes@hotmail.com>
2023-10-18 20:48:58 -0300

    ajuste bredcrumbs e loading

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -4,9 +4,9 @@ import Social from "../components/Social";
 export default function Contacts(){
     return(
         <section className="h-screen bg-[rgba(255, 255, 255, 0.1)] backdrop-blur-md my-4 container mx-auto">
-            <div className="divide-x-[1px] divide-gray-400 flex justify-center my-4">
-                <Link className="p-1 text-gray-200 hover:text-green-600 transition-colors" href={"/"}>Home</Link>
-                <Link className="p-1 text-gray-200 hover:text-green-600 transition-colors" href={"/projects"}>Projetos</Link>
+            <div className="divide-x-[1px] divide-gray-400 flex justify-end my-4">
+                <Link className="p-1 text-gray-300 font-medium hover:text-green-600 transition-colors" href={"/"}>Home</Link>
+                <Link className="p-1 text-gray-300 font-medium hover:text-green-600 transition-colors" href={"/projects"}>Projetos</Link>
             </div>
             <div className="flex items-center flex-col justify-center gap-4 text-gray-400">
                 <Social/>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,8 @@
 
-import  {Repos}  from "../components/Repo"
+import { Repos } from "../components/Repo"
 import { Metadata } from "next"
 import Solution from "../components/Solution"
+import Link from "next/link"
 
 export const metadata: Metadata = {
     title: 'Projects',
@@ -9,18 +10,22 @@ export const metadata: Metadata = {
     keywords: ['GitHub', 'Projetos', 'repositório', 'tecnologias']
 }
 
-export default function Projects(){
+export default function Projects() {
     return (
         <div className="container px-2 mx-auto">
+            <div className="divide-x-[1px] divide-gray-400 flex justify-end my-4">
+                <Link className="p-1 text-gray-300 font-medium hover:text-green-600 transition-colors" title="Ir para" href={"/"}>Home</Link>
+                <Link className="p-1 text-gray-300 font-medium hover:text-green-600 transition-colors" title="Ir para" href={"/contacts"}>Contato</Link>
+            </div>
             <div className="my-4">
                 <div className="my-2 text-gray-200">
                     <h4 className=" text-2xl font-bold">Principais projetos</h4>
-                    <Solution/>
+                    <Solution />
                 </div>
             </div>
             <div>
                 <h4 className="my-2 text-2xl font-bold text-gray-200">Repositório</h4>
-                <Repos/>
+                <Repos />
             </div>
         </div>
     )
